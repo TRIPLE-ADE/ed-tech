eca0f12992fff63892ef6d8c3b5319950526490f
TRIPLE-ADE <adetomiwaabdul@gmail.com>
2025-06-23 14:34:53 +0100

    feat: add environment variable exports for BUCKET_ID and DATABASE_ID

--- lib/appwrite/config.ts
+++ lib/appwrite/config.ts
@@ -8,4 +8,7 @@ export const account = new Account(client)
 export const databases = new Databases(client)
 export const storage = new Storage(client)
 
+export const BUCKET_ID = process.env.NEXT_PUBLIC_APPWRITE_BUCKET_ID || 'default'
+export const DATABASE_ID = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID || 'default'
+
 export { client }
